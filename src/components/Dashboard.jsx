91c042f3db91d08a08eaa31ac38a5e0a1f817d93
Muneeb Baig <109512760+muneeb-hub-23@users.noreply.github.com>
2024-01-23 21:39:22 +0500

    dashboard70%
    dashboard70%

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,18 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SideBAR from './SideBAR';
 import LineChart from './LineChart';
+import BarChart from './BarChart';
+import './dashboard.css'
+import axios from 'axios';
 const Dashboard = () => {
+const [tstrength,setTStrength] = useState({})
+const [tpresent,setPresent] = useState({})
+const [tabsent,setAbsent] = useState({})
+const [tleave,setLeave] = useState({})
+  const ApiCaller = async ()=>{
+    try{
+      const response = await axios.post('http://localhost:80/today-ict-strength', []);
+      setTStrength(response.data)
+    }catch (err){
+      console.log(err)
+    }
+    try{
+      const response = await axios.post('http://localhost:80/today-ict-present', []);
+      setPresent(response.data)
+    }catch (err){
+      console.log(err)
+    }
+    try{
+      const response = await axios.post('http://localhost:80/today-ict-absent', []);
+      setAbsent(response.data)
+    }catch (err){
+      console.log(err)
+    }
+    try{
+      const response = await axios.post('http://localhost:80/today-ict-leave', []);
+      setLeave(response.data)
+    }catch (err){
+      console.log(err)
+    }
+  }
+  ApiCaller()
   return (
     <div>
       <SideBAR>
         <h1>Dashboard</h1>
         <div className="container">
         <div className="row">
-        <div className='col-lg' style={{width:'35%',height:'40vh',border:'1px solid black',margin:'10px'}}>
-<h1>Today ICT</h1>
+          <table style={{margin:'5px 10px 5px 10pxs'}}>
+            <tr>
+         <td style={{width:'43vw'}}><h3 style={{textAlign:'left'}}>Today ICT</h3></td>
+          <td><h3 style={{textAlign:'left'}}>Top Performing Class</h3></td>
+          <td>
+            <select name="" id="">
+              <option value="today">Today</option>
+              <option value="yesterday">Yesterday</option>
+              <option value="this_week">This Week</option>
+              <option value="last_week">Last Week</option>
+              <option value="this_month">This Month</option>
+              <option value="last_month">Last Month</option>
+              <option value="this_year">This Year</option>
+              <option value="last_year">Last Year</option>
+              <option value="all_time">All Time</option>
+            </select>
+            
+            </td>
+          </tr>
+          </table>
+        <div className='col-lg' style={{width:'30vh',height:'45vh',border:'1px solid black',margin:'10px'}}>
+
+        <div className="row">
+
+        <div className="col-sm center">
+        <h3 className='written'>Total Strength</h3>
+        <div className="custom strength">{tstrength.strength}</div>
         </div>
-        <div className='col-lg' style={{width:'35%',height:'40vh',border:'1px solid black',margin:'10px'}}>
-          <h1>Top Class</h1>
+
+        <div className="col-sm center">
+        <h3 className='written'>Total Present</h3>
+        <div className="custom present">{tpresent.present}</div>
+        </div>
+
+        <div className="col-sm center">
+        <h3 className='written'>Total Absent</h3>
+        <div className="custom absent">{tabsent.absent}</div>
+        </div>
+
+        <div className="col-sm center">
+        <h3 className='written'>Total Leaves</h3>
+        <div className="custom leave">{tleave.leaves}</div>
+        </div>
+
+        </div>
+
+
+
+
+          
+        </div>
+  
+        <div className='col-lg' style={{width:'30vh',height:'45vh',border:'1px solid black',margin:'10px'}}>
+        <BarChart data={[50,30,80,40,40,70]}/>
         </div>
         </div>
         <div className="row">
--- src/components/ViewAttendance/ViewAttendence.jsx
+++ src/components/ViewAttendance/ViewAttendence.jsx
@@ -7,25 +7,27 @@ import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import { data } from 'jquery';
 
 const ViewAttendence = () => {
-const [firstyeara,setfirstyeara] = useState({});
-const [firstyearb,setfirstyearb] = useState({});
-const [secondyeara,setsecondyeara] = useState({});
-const [secontyearb,setsecontyearb] = useState({});
-const [thirdyeara,setthirdyeara] = useState({});
-const [thirdyearb,setthirdyearb] = useState({});
+const [firstyeara,setfirstyeara] = useState([]);
+const [firstyearb,setfirstyearb] = useState([]);
+const [secondyeara,setsecondyeara] = useState([]);
+const [secontyearb,setsecontyearb] = useState([]);
+const [thirdyeara,setthirdyeara] = useState([]);
+const [thirdyearb,setthirdyearb] = useState([]);
 const [dates,setdates]=useState({date1:new Date().toISOString().split('T')[0]})
 const navigate = useNavigate();
 const ApiCaller = async () => {
   try {
-    const response = await axios.post('http://localhost:80/view-attendance', {date1:dates.date1,class:'1st-year',section:'a'});
+    const response = await axios.post('http://localhost:80/view-attendance', {date1:dates.date1});
     setfirstyeara(response.data[0]);
     setfirstyearb(response.data[1]);
     setsecondyeara(response.data[2]);
     setsecontyearb(response.data[3]);
     setthirdyeara(response.data[4]);
     setthirdyearb(response.data[5]);
+
   } catch (error) {
     console.log(error);
   }
@@ -51,24 +53,24 @@ const style = {margin:'20px',padding:'5px',border:'1px solid black'}
 <div className="container">
   <div className="row">
 <div className='col-lg' style={style}>
-<DataBox class={'1st-year'} section={'A'} data={[firstyeara.total_strength,firstyeara.total_present,firstyeara.total_absent,firstyeara.total_leave]}/>
+<DataBox class={'1st-year'} section={'A'} data={[firstyeara[0],firstyeara[1],firstyeara[2],firstyeara[3]]}/>
 </div>
 <div className='col-lg' style={style}>
-<DataBox class={'1st-year'} section={'B'} data={[firstyearb.total_strength,firstyearb.total_present,firstyearb.total_absent,firstyearb.total_leave]}/>
+<DataBox class={'1st-year'} section={'B'} data={[firstyearb[0],firstyearb[1],firstyearb[2],firstyearb[3]]}/>
 </div>
 <div className='col-lg' style={style}>
-<DataBox class={'2nd-year'} section={'A'} data={[secondyeara.total_strength,secondyeara.total_present,secondyeara.total_absent,secondyeara.total_leave]}/>
+<DataBox class={'2nd-year'} section={'A'} data={[secondyeara[0],secondyeara[1],secondyeara[2],secondyeara[3]]}/>
 </div>
 </div>
 <div className="row">
 <div className='col-lg' style={style}>
-<DataBox class={'2nd-year'} section={'B'} data={[secontyearb.total_strength,secontyearb.total_present,secontyearb.total_absent,secontyearb.total_leave]}/>
+<DataBox class={'2nd-year'} section={'B'} data={[secontyearb[0],secontyearb[1],secontyearb[2],secontyearb[3]]}/>
 </div>
 <div className='col-lg' style={style}>
-<DataBox class={'3rd-year'} section={'A'} data={[thirdyeara.total_strength,thirdyeara.total_present,thirdyeara.total_absent,thirdyeara.total_leave]}/>
+<DataBox class={'3rd-year'} section={'A'} data={[thirdyeara[0],thirdyeara[1],thirdyeara[2],thirdyeara[3]]}/>
 </div>
 <div className='col-lg' style={style}>
-<DataBox class={'3rd-year'} section={'B'} data={[thirdyearb.total_strength,thirdyearb.total_present,thirdyearb.total_absent,thirdyearb.total_leave]}/>
+<DataBox class={'3rd-year'} section={'B'} data={[thirdyearb[0],thirdyearb[1],thirdyearb[2],thirdyearb[3]]}/>
 </div>
 </div>
 </div>
